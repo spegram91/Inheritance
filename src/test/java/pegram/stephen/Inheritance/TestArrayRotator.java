f4efc5fce157fbbf60e870a39ca593a5b1e61d8f
Stephen Pegram <stephenpegram@Stephens-MacBook-Pro.local>
2017-05-04 19:02:45 -0400

    worked on Rotate-array

---
+++ src/test/java/pegram/stephen/Inheritance/TestArrayRotator.java
@@ -0,0 +1,41 @@
+package pegram.stephen.Inheritance;
+
+/**
+ * Created by stephenpegram on 5/4/17.
+ */
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
+
+//public class TestArrayRotator {
+//
+//    @Test
+//    public void testFillArrayList() {
+//        //Given
+//
+//        boolean expectedValue = true;
+//
+//        //When
+//        boolean actualValue = (testArray.get(testArray.size() - 1) != null);
+//
+//        //Then
+//        Assert.assertEquals("Array list values should be filled in order to rotate them", expectedValue, actualValue);
+//
+//    }
+//
+//
+//    @Test
+//    public void rotateArray() {
+//        //Given
+//
+//
+//        //When
+//
+//        //Then
+//        Assert.assertEquals("", expectedValue, actualValue);
+//
+//    }
+//}
